f6411a0bf085bd856224a707764132671d248216
wangtao <wangTAO123456HAHAOUTLOOK.COMOM>
2020-12-08 21:58:08 +0800

    fjjfgl

--- lodash/wangtao10086-lodash.js
+++ lodash/wangtao10086-lodash.js
@@ -103,6 +103,69 @@ var wangtao10086 = function (){
     return result
   }
 
+  //fill  填充数组 
+  function fill(array,value, start,end) {
+    if(!start || !end) {
+      for(let i = 0; i < array.length ; i++) {
+        array[i] = value
+      }
+    }
+
+    if(start && end) {
+      for(let i = start;i < end ;i++ ) {
+        array[i] = value
+      }
+    }
+
+    return array
+    
+  }
+
+  //findIndex  返回第一个通过predicate判断为真的元素的索引值
+  function findIndex(array,pred) {
+    for(var i = 0;i<array.length;i++) {
+      for(var event in array[i]) {
+        if(array[i][event] == pred) {
+          return i
+        }
+        if(array[i][event] == pred[pred.length - 1] ) {
+          return i
+        }
+      }
+    }
+    return -1
+  }
+
+  //findLastIndex 
+  
+
+  //flatten 较少一级array嵌套深度
+  function flatten(array) {
+    var result = []
+    for(var i = 0 ; i<array.length ; i++) {
+      if(Array.isArray(array[i])) {
+        result.push(...array[i])
+      } else {
+        result.push(array[i])
+      }
+    }
+    return result
+  }
+
+  //flattenDeep 递归为一维数组
+
+  
+  //fromPairs
+  function fromPairs(pairs) {
+    let map = {}
+    for(let i = 0 ; i < pairs.length ; i++) {
+      map[pairs[i][0]] = pairs[i][1]
+    }
+    return map
+  }
+
+  //head
+
 
 
 
@@ -116,6 +179,11 @@ var wangtao10086 = function (){
     lastIndexOf,
     drop,
     dropright,
+    fill,
+    findIndex,
+    flatten,
+    fromPairs,
+    
 
 
   }
