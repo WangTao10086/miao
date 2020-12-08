5f200e6a9f8477a84f184408f692545166789299
wangtao <wangTAO123456HAHAOUTLOOK.COMOM>
2020-12-08 15:18:35 +0800

    bijfi

---
+++ lodash/wangtao10086-lodash.js.js
@@ -0,0 +1,18 @@
+var wangtao10086 = function (){
+  function compact (array) {
+    var result = []
+    for(var i = 0;i<array.length;i++) {
+      if(!array[i]) {
+        return false
+      }
+      retult = result.push[array[i]]
+    }
+    return  result
+  }
+
+
+
+  return {
+    compact,
+  }
+}()
