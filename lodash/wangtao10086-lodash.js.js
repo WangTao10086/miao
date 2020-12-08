d79c21ce1ff0427beb6506e635421eac9bf9942b
wangtao <wangTAO123456HAHAOUTLOOK.COMOM>
2020-12-08 15:34:09 +0800

    hfh

--- lodash/wangtao10086-lodash.js.js
+++ lodash/wangtao10086-lodash.js.js
@@ -1,18 +1,29 @@
+
+//v1: 
+//v2:
+//v3:
+
+
+
 var wangtao10086 = function (){
-  function compact (array) {
+  function compact_v1(array) {
     var result = []
     for(var i = 0;i<array.length;i++) {
       if(!array[i]) {
-        return false
+        continue
       }
-      retult = result.push[array[i]]
+     result.push(array[i])
     }
     return  result
   }
 
 
 
+
+
+
   return {
-    compact,
+    compact_v1,
+
   }
 }()
